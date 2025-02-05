16798ab419899ad25b7466f3439b9e32d3b295fe
Bhintuna <bhintunash@gmail.com>
2025-02-05 11:13:21 +0545

    Model Validation

--- Models/StudentDTO.cs
+++ Models/StudentDTO.cs
@@ -1,10 +1,17 @@
-﻿namespace WebAPI_Learn.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI_Learn.Models
 {
     public class StudentDTO
     {
+        [Required]
         public int ID { get; set; }
         public string StudentName { get; set; }
         public int Roll { get; set; }
         public string Symbol { get; set; }
+
+        //[EmailAddress]
+        //public string Email { get; set; }
+
     }
 }
