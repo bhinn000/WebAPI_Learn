852beffa244f7a3d63fe1666cec8bc437e3e9d14
Bhintuna <bhintunash@gmail.com>
2025-02-07 16:22:09 +0545

    custom middleware

---
+++ MiddleWare/CustomMiddleware.cs
@@ -0,0 +1,34 @@
+﻿
+using Microsoft.AspNetCore.Builder;
+
+namespace WebAPI_Learn.MiddleWare
+{
+    public class Custom1Middleware : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            Console.WriteLine("Hello I am from custom 1 middleware");
+            //await context.Response.WriteAsync("Hello I am from custom 1 middleware");
+            await next(context);
+        }
+    }
+
+    public class Custom2Middleware : IMiddleware //extension method is not there for this class
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            Console.WriteLine("Hello I am from custom 2 middleware");
+            //await context.Response.WriteAsync("Hello I am from custom 2 middleware");
+            await next(context);//next() call in middleware refers to the next middleware 
+        }
+    }
+
+    public static class ClassWithNoImplementationMiddleware
+    {
+        public static IApplicationBuilder UseCustomMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<Custom1Middleware>();
+        }
+    }
+}
+    
--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using Serilog;
 using WebAPI_Learn.Data;
 using WebAPI_Learn.MyLoggings;
 using Microsoft.EntityFrameworkCore;
+using WebAPI_Learn.MiddleWare;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Logging.ClearProviders(); //by default , it include console , debug and event window
@@ -19,6 +20,8 @@ builder.Services.AddScoped<IMyLoggings, LogToFile>(); //registering custom servi
 //builder.Services.AddScoped<IMyLoggings, LogToDB>();
 //builder.Services.AddScoped<IMyLoggings, LogToServerMemory>();
 
+builder.Services.AddTransient<Custom2Middleware>();//without using Extension Method --1.1
+
 //AddSingleton , 1 number of object of DI engine is created
 //AddTransient , n number of object of DI engine is created
 //AddScoped , n*n number of object of DI engine is created for n request and n call for di engine
@@ -29,6 +32,8 @@ Log.Logger = new LoggerConfiguration().MinimumLevel.Information()
 //builder.Host.UseSerilog();//this will allow to write in file only , cant log to console
 //to allow to log to console or debug too , you can do following:
 builder.Logging.AddSerilog();
+
+builder.Services.AddScoped<Custom1Middleware>(); //adding custom middleware using addscoped
 
 builder.Services.AddDbContext<CollegeDBContext>(
     options=>
@@ -42,9 +47,13 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseCustomMiddleware();//using extension method
+    app.UseMiddleware<Custom2Middleware>();//without using Extension Method --1.2
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+//app.UseMiddleware<Custom2Middleware>();//without using Extension Method --1.2
 
 app.UseHttpsRedirection();
 
