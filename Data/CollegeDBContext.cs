183532893b3de1f188e12497a8e59f4652cfefcc
Bhintuna <bhintunash@gmail.com>
2025-02-10 12:00:37 +0545

    Moving dbcontext configuration to separate file

--- Data/CollegeDBContext.cs
+++ Data/CollegeDBContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using WebAPI_Learn.Data.Config;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace WebAPI_Learn.Data
@@ -13,19 +14,23 @@ namespace WebAPI_Learn.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Student>().HasData(new List<Student>() { 
-                new Student{ID=1 , StudentName="Bhintuna" ,Email="b@gmail.com" , Address="Nepal"} ,
-                new Student{ID=2 , StudentName="Shakya" ,Email="b@gmail.com" , Address="Nepal2"}
-            });
+            //using this , dbcontext will know about configuration file 
+            modelBuilder.ApplyConfiguration(new StudentConfig());//adding configuration to db context
+
+            //modelBuilder.Entity<Student>().HasData(new List<Student>() { 
+            //    new Student{ID=1 , StudentName="Bhintuna" ,Email="b@gmail.com" , Address="Nepal"} ,
+            //    new Student{ID=2 , StudentName="Shakya" ,Email="b@gmail.com" , Address="Nepal2"}
+            //});
 
             //modifying schema using EF code first method
-            modelBuilder.Entity<Student>(entity =>
-            {
-                entity.Property(n => n.StudentName).IsRequired();
-                entity.Property(n => n.StudentName).IsRequired();
-                entity.Property(n => n.Address).IsRequired(false).HasMaxLength(500);
-                entity.Property(n => n.Email).IsRequired(false).HasMaxLength(500);
-            });
+            //now this is done in configuration StudentConfig.cs
+            //modelBuilder.Entity<Student>(entity =>
+            //{
+            //    entity.Property(n => n.StudentName).IsRequired();
+            //    entity.Property(n => n.StudentName).IsRequired();
+            //    entity.Property(n => n.Address).IsRequired(false).HasMaxLength(500);
+            //    entity.Property(n => n.Email).IsRequired(false).HasMaxLength(500);
+            //});
         }
     }
 }
---
+++ Data/Config/StudentConfig.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace WebAPI_Learn.Data.Config
+{
+    public class StudentConfig : IEntityTypeConfiguration<Student>
+    {
+        public void Configure(EntityTypeBuilder<Student> builder)
+        {
+            builder.ToTable("StudentsTable");
+            builder.HasKey(x => x.ID);//make primary key to ID
+            builder.Property(x => x.ID).UseIdentityColumn(); // auto increment new entities 
+            builder.Property(n => n.StudentName).IsRequired();
+            builder.Property(n => n.StudentName).IsRequired();
+            builder.Property(n => n.Address).IsRequired(false).HasMaxLength(500);
+            builder.Property(n => n.Email).IsRequired(false).HasMaxLength(500);
+
+            builder.HasData(new List<Student>() {
+                new Student{ID=1 , StudentName="Bhintuna" ,Email="b@gmail.com" , Address="Nepal"} ,
+                new Student{ID=2 , StudentName="Shakya" ,Email="b@gmail.com" , Address="Nepal2"}
+            });
+
+        }
+    }
+}
+    
